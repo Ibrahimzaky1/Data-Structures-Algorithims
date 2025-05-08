95c02167eb99ac2922c55aebfdc4c238fc59c6b2
Ibrahim <ibrahimzaky2000@gmail.com>
2025-05-08 17:27:09 +0200

    Updated firstpage.py

--- firstpage.py
+++ firstpage.py
@@ -1,18 +1,134 @@
 ######### LESSON 3 #########
-expenses = [2200, 2350, 2600, 2130, 2190]
+class Node:
+    def __init__(self, data=None, next=None, prev=None):
+        self.data = data
+        self.next = next
+        self.prev = prev
 
-print("extra spent in Febuary compared to January is:", expenses[1] - expenses[0])
-print("total expenses in first quarter(first three months) is:", expenses[0] + expenses[1] + expenses[2])
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
 
-if 2000 in expenses:
-    print(" you spent excactlly 2000 in one of the months ")
+    def print_forward(self):
+        if self.head is None:
+            print("Linked list is empty")
+            return
 
-else:
-    print(" no month has excalctly 2000 in expenses ")
+        itr = self.head
+        llstr = ''
+        while itr:
+            llstr += str(itr.data) + ' --> '
+            itr = itr.next
+        print(llstr)
 
-expenses.append("1980")
-print(expenses)
+    def print_backward(self):
+        if self.head is None:
+            print("Linked list is empty")
+            return
 
-expenses[3] -= 200
-print(expenses)
+        last_node = self.get_last_node()
+        itr = last_node
+        llstr = ''
+        while itr:
+            llstr += itr.data + '-->'
+            itr = itr.prev
+        print("Link list in reverse: ", llstr)
 
+    def get_last_node(self):
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+
+        return itr
+
+    def get_length(self):
+        count = 0
+        itr = self.head
+        while itr:
+            count+=1
+            itr = itr.next
+
+        return count
+
+    def insert_at_begining(self, data):
+        if self.head == None:
+            node = Node(data, self.head, None)
+            self.head = node
+        else:
+            node = Node(data, self.head, None)
+            self.head.prev = node
+            self.head = node
+
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.head = Node(data, None, None)
+            return
+
+        itr = self.head
+
+        while itr.next:
+            itr = itr.next
+
+        itr.next = Node(data, None, itr)
+
+    def insert_at(self, index, data):
+        if index<0 or index>self.get_length():
+            raise Exception("Invalid Index")
+
+        if index==0:
+            self.insert_at_begining(data)
+            return
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                node = Node(data, itr.next, itr)
+                if node.next:
+                    node.next.prev = node
+                itr.next = node
+                break
+
+            itr = itr.next
+            count += 1
+
+    def remove_at(self, index):
+        if index<0 or index>=self.get_length():
+            raise Exception("Invalid Index")
+
+        if index==0:
+            self.head = self.head.next
+            self.head.prev = None
+            return
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index:
+                itr.prev.next = itr.next
+                if itr.next:
+                    itr.next.prev = itr.prev
+                break
+
+            itr = itr.next
+            count+=1
+
+    def insert_values(self, data_list):
+        self.head = None
+        for data in data_list:
+            self.insert_at_end(data)
+
+
+if __name__ == '__main__':
+    ll = DoublyLinkedList()
+    ll.insert_values(["banana","mango","grapes","orange"])
+    ll.print_forward()
+    ll.print_backward()
+    ll.insert_at_end("figs")
+    ll.print_forward()
+    ll.insert_at(0,"jackfruit")
+    ll.print_forward()
+    ll.insert_at(6,"dates")
+    ll.print_forward()
+    ll.insert_at(2,"kiwi")
+    ll.print_forward()
