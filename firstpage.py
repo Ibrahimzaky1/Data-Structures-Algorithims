e73d2654846de6d05dd140b47347494b4af5e398
Ibrahim <ibrahimzaky2000@gmail.com>
2025-05-08 13:39:33 +0200

    Updated firstpage.py

--- firstpage.py
+++ firstpage.py
@@ -1,99 +1,18 @@
+######### LESSON 3 #########
+expenses = [2200, 2350, 2600, 2130, 2190]
 
-############# LINKED LIST #############
+print("extra spent in Febuary compared to January is:", expenses[1] - expenses[0])
+print("total expenses in first quarter(first three months) is:", expenses[0] + expenses[1] + expenses[2])
 
-class Node:
-    def __init__(self, data=None, next=None):
-        self.data = data
-        self.next = next
+if 2000 in expenses:
+    print(" you spent excactlly 2000 in one of the months ")
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
-        
-    def insert_at_begining(self, data):
-        node = Node(data, self.head)
-        self.head = node
+else:
+    print(" no month has excalctly 2000 in expenses ")
 
-    def print(self):
-        if self.head is None:
-            print("Linked List is empty")
-            return
-        
-        itr = self.head
-        llstr = ''
-        while itr:
-            llstr += str(itr.data) + '-->'
-            itr = itr.next
-        print(llstr)
+expenses.append("1980")
+print(expenses)
 
-    def insert_at_end(self, data):
-        if self.head is None:
-            self.head = Node(data, None)
-            return
-        itr = self.head
-        while itr.next:
-            itr = itr.next
-
-        itr.next = Node(data, None)
-
-    def insert_values(self, data_list):
-        self.head = None
-        for data in data_list:
-            self.insert_at_end(data)
-
-    def get_lengh(self):
-        count = 0
-        itr = self.head
-        while itr:
-            count+=1
-            itr = itr.next
-
-        return count
-    def remove_at(self, index):
-        if index<0 or index>=self.get_lengh():
-            raise Exception("Invalid index")
-        
-        if index==0:
-            self.head = self.head.next
-            return
-        
-        count = 0
-        itr = self.head
-        while itr:
-            if count == index - 1:
-                itr.next = itr.next.next
-                break
-            itr = itr.next
-            count += 1
-
-    def insert_at(self, index, data):
-        if index<0 or index>self.get_lengh():
-            raise Exception("Invalid Index")
-        
-        if index==0:
-            self.insert_at_begining(data)
-            return
-        
-        count = 0
-        itr = self.head
-        while itr:
-            if count == index -1:
-                node = Node(data, itr.next)
-                itr.next = node
-                break
-
-            itr = itr.next
-            count += 1
-
-    
-if __name__ == '__main__':
-    ll = LinkedList()
-    ll.insert_values(["banana", "mango", "grapes", "orange"])
-    ll.print()
-    ll.insert_at(0, "figs")
-    ll.print()
-    ll.insert_at(2, "jackfruit")
-    ll.print()
-        
-
+expenses[3] -= 200
+print(expenses)
 
