c2fec674d8040cdde8f1745afb416a875e64b3a0
Ibrahim <ibrahimzaky2000@gmail.com>
2025-05-08 13:01:40 +0200

    Updated firstpage.py

--- firstpage.py
+++ firstpage.py
@@ -1,20 +1,99 @@
-def find_first_pe(prices, eps, index):
-    pe = prices[index]/eps[index]
-    return pe
 
-numbers = [3, 6, 2, 4, 3, 6, 8, 9]
-duplicate = None
-for i in range(len(numbers)):
-    for j in range(i+1, len(numbers)):
-        if numbers[i] == numbers[j]:
-            print(numbers[i] + " is a dupliacate")
-            break
+############# LINKED LIST #############
 
-for i in range(len(numbers)):
-    if numbers[i] == duplicate:
-        print(i)
+class Node:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
 
-for i in range(len(numbers)):
-    if numbers[i] == 68:
-        print(i)
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        
+    def insert_at_begining(self, data):
+        node = Node(data, self.head)
+        self.head = node
+
+    def print(self):
+        if self.head is None:
+            print("Linked List is empty")
+            return
+        
+        itr = self.head
+        llstr = ''
+        while itr:
+            llstr += str(itr.data) + '-->'
+            itr = itr.next
+        print(llstr)
+
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.head = Node(data, None)
+            return
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+
+        itr.next = Node(data, None)
+
+    def insert_values(self, data_list):
+        self.head = None
+        for data in data_list:
+            self.insert_at_end(data)
+
+    def get_lengh(self):
+        count = 0
+        itr = self.head
+        while itr:
+            count+=1
+            itr = itr.next
+
+        return count
+    def remove_at(self, index):
+        if index<0 or index>=self.get_lengh():
+            raise Exception("Invalid index")
+        
+        if index==0:
+            self.head = self.head.next
+            return
+        
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                itr.next = itr.next.next
+                break
+            itr = itr.next
+            count += 1
+
+    def insert_at(self, index, data):
+        if index<0 or index>self.get_lengh():
+            raise Exception("Invalid Index")
+        
+        if index==0:
+            self.insert_at_begining(data)
+            return
+        
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index -1:
+                node = Node(data, itr.next)
+                itr.next = node
+                break
+
+            itr = itr.next
+            count += 1
+
+    
+if __name__ == '__main__':
+    ll = LinkedList()
+    ll.insert_values(["banana", "mango", "grapes", "orange"])
+    ll.print()
+    ll.insert_at(0, "figs")
+    ll.print()
+    ll.insert_at(2, "jackfruit")
+    ll.print()
+        
+
 
