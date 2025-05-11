3f09b3197b8a9f32195cef1fa7de2f8a8d321a8c
Ibrahim <ibrahimzaky2000@gmail.com>
2025-05-11 16:26:13 +0200

    Updated firstpage.py

--- firstpage.py
+++ firstpage.py
@@ -1,271 +1,5 @@
-#################### 7 LESSON ####################
 
-from collections import deque
-class Stack:
-    def __init__(self):
-        self.container = deque()
-
-    def push(self, val):
-        self.container.append(val)
-
-    def pop(self):
-        return self.container.pop()
-
-    def peek(self):
-        return self.container[-1]
-
-    def is_empty(self):
-        return len(self.container) == 0
-
-    def size(self):
-        return len(self.container)
-
-def reverse_string(s):
-    stack = Stack()
-
-    for ch in s:
-        stack.push(ch)
-
-    rstr = ''
-    while stack.size()!=0:
-        rstr += stack.pop()
-
-    return rstr
-
-
-if __name__ == '__main__':
-    print(reverse_string("We will conquere COVI-19"))
-    print(reverse_string("I am the king"))
-
-
-
-
-from collections import deque
-
-class Stack:
-    def __init__(self):
-        self.container = deque()
-
-    def push(self, val):
-        self.container.append(val)
-
-    def pop(self):
-        return self.container.pop()
-
-    def peek(self):
-        return self.container[-1]
-
-    def is_empty(self):
-        return len(self.container) == 0
-
-    def size(self):
-        return len(self.container)
-
-def is_match(ch1, ch2):
-    match_dict = {
-        ')': '(',
-        ']': '[',
-        '}': '{'
-    }
-    return match_dict[ch1] == ch2
-
-
-def is_balanced(s):
-    stack = Stack()
-    for ch in s:
-        if ch=='(' or ch=='{' or ch == '[':
-            stack.push(ch)
-        if ch==')' or ch=='}' or ch == ']':
-            if stack.size()==0:
-                return False
-            if not is_match(ch,stack.pop()):
-                return False
-
-    return stack.size()==0
-
-
-if __name__ == '__main__':
-    print(is_balanced("({a+b})"))
-    print(is_balanced("))((a+b}{"))
-    print(is_balanced("((a+b))"))
-    print(is_balanced("((a+g))"))
-    print(is_balanced("))"))
-    print(is_balanced("[a+b]*(x+2y)*{gg+kk}"))
-
-
-#################### 8 LESSON ####################
-
-import threading
-import time
-
-from collections import deque
-
-class Queue:
-    def __init__(self):
-        self.buffer = deque()
-
-    def enqueue(self, val):
-        self.buffer.appendleft(val)
-
-    def dequeue(self):
-        if len(self.buffer)==0:
-            print("Queue is empty")
-            return
-
-        return self.buffer.pop()
-
-    def is_empty(self):
-        return len(self.buffer) == 0
-
-    def size(self):
-        return len(self.buffer)
-
-food_order_queue = Queue()
-
-def place_orders(orders):
-    for order in orders:
-        print("Placing order for:",order)
-        food_order_queue.enqueue(order)
-        time.sleep(0.5)
-
-
-def serve_orders():
-    time.sleep(1)
-    while True:
-        order = food_order_queue.dequeue()
-        print("Now serving: ",order)
-        time.sleep(2)
-
-if __name__ == '__main__':
-    orders = ['pizza','samosa','pasta','biryani','burger']
-    t1 = threading.Thread(target=place_orders, args=(orders,))
-    t2 = threading.Thread(target=serve_orders)
-
-    t1.start()
-    t2.start()
-
-    #################### 9 LESSON ####################
-
-class TreeNode:
-    def __init__(self, data):
-        self.data = data
-        self.children = []
-        self.parent = None
-
-    def get_level(self):
-        level = 0
-        p = self.parent
-        while p:
-            level += 1
-            p = p.parent
-
-        return level
-
-    def print_tree(self):
-        spaces = ' ' * self.get_level() * 3
-        prefix = spaces + "|__" if self.parent else ""
-        print(prefix + self.data)
-        if self.children:
-            for child in self.children:
-                child.print_tree()
-
-    def add_child(self, child):
-        child.parent = self
-        self.children.append(child)
-
-def build_product_tree():
-    root = TreeNode("Electronics")
-
-    laptop = TreeNode("Laptop")
-    laptop.add_child(TreeNode("Mac"))
-    laptop.add_child(TreeNode("Surface"))
-    laptop.add_child(TreeNode("Thinkpad"))
-
-    cellphone = TreeNode("Cell Phone")
-    cellphone.add_child(TreeNode("iPhone"))
-    cellphone.add_child(TreeNode("Google Pixel"))
-    cellphone.add_child(TreeNode("Vivo"))
-
-    tv = TreeNode("TV")
-    tv.add_child(TreeNode("Samsung"))
-    tv.add_child(TreeNode("LG"))
-
-    root.add_child(laptop)
-    root.add_child(cellphone)
-    root.add_child(tv)
-
-    root.print_tree()
-
-if __name__ == '__main__':
-    build_product_tree()
-
-
-class TreeNode:
-    def __init__(self, name, designation):
-        self.name = name
-        self.designation = designation
-        self.children = []
-        self.parent = None
-
-    def get_level(self):
-        level = 0
-        p = self.parent
-        while p:
-            level += 1
-            p = p.parent
-
-        return level
-
-    def print_tree(self, property_name):
-        if property_name == 'both':
-            value = self.name + " (" + self.designation + ")"
-        elif property_name == 'name':
-            value = self.name
-        else:
-            value = self.designation
-
-        spaces = ' ' * self.get_level() * 3
-        prefix = spaces + "|__" if self.parent else ""
-        print(prefix + value)
-        if self.children:
-            for child in self.children:
-                child.print_tree(property_name)
-
-    def add_child(self, child):
-        child.parent = self
-        self.children.append(child)
-
-def build_management_tree():
-    # CTO Hierarchy
-    infra_head = TreeNode("Vishwa","Infrastructure Head")
-    infra_head.add_child(TreeNode("Dhaval","Cloud Manager"))
-    infra_head.add_child(TreeNode("Abhijit", "App Manager"))
-
-    cto = TreeNode("Chinmay", "CTO")
-    cto.add_child(infra_head)
-    cto.add_child(TreeNode("Aamir", "Application Head"))
-
-    # HR hierarchy
-    hr_head = TreeNode("Gels","HR Head")
-
-    hr_head.add_child(TreeNode("Peter","Recruitment Manager"))
-    hr_head.add_child(TreeNode("Waqas", "Policy Manager"))
-
-    ceo = TreeNode("Nilupul", "CEO")
-    ceo.add_child(cto)
-    ceo.add_child(hr_head)
-
-    return ceo
-
-
-if __name__ == '__main__':
-    root_node = build_management_tree()
-    root_node.print_tree("name")
-    root_node.print_tree("designation")
-    root_node.print_tree("both")
-
-
-    #################### 9 LESSON ####################
+#################### LESSON 11 ####################
 
 class BinarySearchTreeNode:
     def __init__(self, data):
@@ -317,25 +51,26 @@ class BinarySearchTreeNode:
 
         return elements
 
-    def post_order_traversal(self):
-        elements = []
-        if self.left:
-            elements += self.left.post_order_traversal()
-        if self.right:
-            elements += self.right.post_order_traversal()
+    def delete(self, val):
+        if val < self.data:
+            if self.left:
+                self.left = self.left.delete(val)
+        elif val > self.data:
+            if self.right:
+                self.right = self.right.delete(val)
+        else:
+            if self.left is None and self.right is None:
+                return None
+            elif self.left is None:
+                return self.right
+            elif self.right is None:
+                return self.right
 
-        elements.append(self.data)
+            max_val = self.left.find_max()
+            self.data = max_val
+            self.left = self.left.delete(max_val)
 
-        return elements
-
-    def pre_order_traversal(self):
-        elements = [self.data]
-        if self.left:
-            elements += self.left.pre_order_traversal()
-        if self.right:
-            elements += self.right.pre_order_traversal()
-
-        return elements
+        return self
 
     def find_max(self):
         if self.right is None:
@@ -347,12 +82,9 @@ class BinarySearchTreeNode:
             return self.data
         return self.left.find_min()
 
-    def calculate_sum(self):
-        left_sum = self.left.calculate_sum() if self.left else 0
-        right_sum = self.right.calculate_sum() if self.right else 0
-        return self.data + left_sum + right_sum
 
 def build_tree(elements):
+    print("Building tree with these elements:",elements)
     root = BinarySearchTreeNode(elements[0])
 
     for i in range(1,len(elements)):
@@ -361,15 +93,239 @@ def build_tree(elements):
     return root
 
 if __name__ == '__main__':
-    numbers = [17, 4, 1, 20, 9, 23, 18, 34]
+    numbers_tree = build_tree([17, 4, 1, 20, 9, 23, 18, 34])
+    numbers_tree.delete(20)
+    print("After deleting 20 ",numbers_tree.in_order_traversal()) # this should print [1, 4, 9, 17, 18, 23, 34]
 
-    numbers = [15,12,7,14,27,20,23,88 ]
+    numbers_tree = build_tree([17, 4, 1, 20, 9, 23, 18, 34])
+    numbers_tree.delete(9)
+    print("After deleting 9 ",numbers_tree.in_order_traversal())  # this should print [1, 4, 17, 18, 20, 23, 34]
 
-    numbers_tree = build_tree(numbers)
-    print("Input numbers:",numbers)
-    print("Min:",numbers_tree.find_min())
-    print("Max:",numbers_tree.find_max())
-    print("Sum:", numbers_tree.calculate_sum())
-    print("In order traversal:", numbers_tree.in_order_traversal())
-    print("Pre order traversal:", numbers_tree.pre_order_traversal())
-    print("Post order traversal:", numbers_tree.post_order_traversal())
+    numbers_tree = build_tree([17, 4, 1, 20, 9, 23, 18, 34])
+    numbers_tree.delete(17)
+    print("After deleting 17 ",numbers_tree.in_order_traversal())  # this should print [1, 4, 9, 18, 20, 23, 34]
+
+
+#################### LESSON 12 ####################
+
+class Graph:
+    def __init__(self, edges):
+        self.edges = edges
+        self.graph_dict = {}
+        for start, end in edges:
+            if start in self.graph_dict:
+                self.graph_dict[start].append(end)
+            else:
+                self.graph_dict[start] = [end]
+        print("Graph Dict:", self.graph_dict)
+
+    def get_paths(self, start, end, path=[]):
+        path = path + [start]
+
+        if start == end:
+            return [path]
+
+        if start not in self.graph_dict:
+            return []
+
+        paths = []
+        for node in self.graph_dict[start]:
+            if node not in path:
+                new_paths = self.get_paths(node, end, path)
+                for p in new_paths:
+                    paths.append(p)
+
+        return paths
+
+    def get_shortest_path(self, start, end, path=[]):
+        path = path + [start]
+
+        if start == end:
+            return path
+
+        if start not in self.graph_dict:
+            return None
+
+        shortest_path = None
+        for node in self.graph_dict[start]:
+            if node not in path:
+                sp = self.get_shortest_path(node, end, path)
+                if sp:
+                    if shortest_path is None or len(sp) < len(shortest_path):
+                        shortest_path = sp
+
+        return shortest_path
+
+if __name__ == '__main__':
+
+    routes = [
+        ("Mumbai","Pune"),
+        ("Mumbai", "Surat"),
+        ("Surat", "Bangaluru"),
+        ("Pune","Hyderabad"),
+        ("Pune","Mysuru"),
+        ("Hyderabad","Bangaluru"),
+        ("Hyderabad", "Chennai"),
+        ("Mysuru", "Bangaluru"),
+        ("Chennai", "Bangaluru")
+    ]
+
+    routes = [
+        ("Mumbai", "Paris"),
+        ("Mumbai", "Dubai"),
+        ("Paris", "Dubai"),
+        ("Paris", "New York"),
+        ("Dubai", "New York"),
+        ("New York", "Toronto"),
+    ]
+
+    route_graph = Graph(routes)
+
+    start = "Mumbai"
+    end = "New York"
+
+    print(f"All paths between: {start} and {end}: ",route_graph.get_paths(start,end))
+    print(f"Shortest path between {start} and {end}: ", route_graph.get_shortest_path(start,end))
+
+    start = "Dubai"
+    end = "New York"
+
+    print(f"All paths between: {start} and {end}: ",route_graph.get_paths(start,end))
+    print(f"Shortest path between {start} and {end}: ", route_graph.get_shortest_path(start,end))
+
+
+#################### LESSON 13 ####################
+
+from util import time_it
+
+@time_it
+def linear_search(numbers_list, number_to_find):
+    for index, element in enumerate(numbers_list):
+        if element == number_to_find:
+            return index
+    return -1
+
+@time_it
+def binary_search(numbers_list, number_to_find):
+    left_index = 0
+    right_index = len(numbers_list) - 1
+    mid_index = 0
+
+    while left_index <= right_index:
+        mid_index = (left_index + right_index) // 2
+        mid_number = numbers_list[mid_index]
+
+        if mid_number == number_to_find:
+            return mid_index
+
+        if mid_number < number_to_find:
+            left_index = mid_index + 1
+        else:
+            right_index = mid_index - 1
+
+    return -1
+
+def binary_search_recursive(numbers_list, number_to_find, left_index, right_index):
+    if right_index < left_index:
+        return -1
+
+    mid_index = (left_index + right_index) // 2
+    if mid_index >= len(numbers_list) or mid_index < 0:
+        return -1
+
+    mid_number = numbers_list[mid_index]
+
+    if mid_number == number_to_find:
+        return mid_index
+
+    if mid_number < number_to_find:
+        left_index = mid_index + 1
+    else:
+        right_index = mid_index - 1
+
+    return binary_search_recursive(numbers_list, number_to_find, left_index, right_index)
+
+if __name__ == '__main__':
+    numbers_list = [12, 15, 17, 19, 21, 24, 45, 67]
+    number_to_find = 21
+
+    index = binary_search_recursive(numbers_list, number_to_find, 0, len(numbers_list))
+    print(f"Number found at index {index} using binary search")
+
+
+
+
+
+
+
+def binary_search(numbers_list, number_to_find):
+    left_index = 0
+    right_index = len(numbers_list) - 1
+    mid_index = 0
+
+    while left_index <= right_index:
+        mid_index = (left_index + right_index) // 2
+        mid_number = numbers_list[mid_index]
+
+        if mid_number == number_to_find:
+            return mid_index
+
+        if mid_number < number_to_find: # this means number is in right hand side of the list
+            left_index = mid_index + 1
+        else: # number to find is on left hand side of the list
+            right_index = mid_index - 1
+
+    return -1
+
+def find_all_occurances(numbers, number_to_find):
+    index = binary_search(numbers, number_to_find)
+    indices = [index]
+    # find indices on left hand side
+    i = index-1
+    while i >=0:
+        if numbers[i] == number_to_find:
+            indices.append(i)
+        else:
+            break
+        i = i - 1
+
+    # find indices on right hand side
+    i = index + 1
+    while i<len(numbers):
+        if numbers[i] == number_to_find:
+            indices.append(i)
+        else:
+            break
+        i = i + 1
+
+    return sorted(indices)
+
+
+if __name__ == '__main__':
+    numbers = [1,4,6,9,11,15,15,15,17,21,34,34,56]
+    number_to_find = 15
+    indices = find_all_occurances(numbers, number_to_find)
+    print(f"Indices of occurances of {number_to_find} are {indices}")
+
+#################### LESSON 14 ####################
+
+def bubble_sort(elements):
+    size = len(elements)
+    for i in range(size-1):
+        swapped = False
+        for j in range(size-1-i):
+
+            if elements[j] > elements[j+1]:
+                tmp = elements[j]
+                elements[j] = elements[j+1]
+                elements[j+1] = tmp
+                swapped = True
+        if not swapped:
+            break
+
+if __name__ == '__main__':
+    #elements = [5,9,2,1,67,34,88,34]
+    elements = [1,2,4]
+
+    bubble_sort(elements)
+    print(elements)
