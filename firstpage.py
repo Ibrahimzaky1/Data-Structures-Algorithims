061e1243a3624852321d8c9c83f1020afd955c5f
Ibrahim <ibrahimzaky2000@gmail.com>
2025-05-12 06:41:28 +0200

    Updated firstpage.py

--- firstpage.py
+++ firstpage.py
@@ -1,331 +1,59 @@
+######################## LESSON 15 ########################
 
-#################### LESSON 11 ####################
 
-class BinarySearchTreeNode:
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
 
-    def add_child(self, data):
-        if data == self.data:
-            return # node already exist
 
-        if data < self.data:
-            if self.left:
-                self.left.add_child(data)
-            else:
-                self.left = BinarySearchTreeNode(data)
-        else:
-            if self.right:
-                self.right.add_child(data)
-            else:
-                self.right = BinarySearchTreeNode(data)
+######################## LESSON 16 ########################
 
-
-    def search(self, val):
-        if self.data == val:
-            return True
-
-        if val < self.data:
-            if self.left:
-                return self.left.search(val)
-            else:
-                return False
-
-        if val > self.data:
-            if self.right:
-                return self.right.search(val)
-            else:
-                return False
-
-    def in_order_traversal(self):
-        elements = []
-        if self.left:
-            elements += self.left.in_order_traversal()
-
-        elements.append(self.data)
-
-        if self.right:
-            elements += self.right.in_order_traversal()
-
-        return elements
-
-    def delete(self, val):
-        if val < self.data:
-            if self.left:
-                self.left = self.left.delete(val)
-        elif val > self.data:
-            if self.right:
-                self.right = self.right.delete(val)
-        else:
-            if self.left is None and self.right is None:
-                return None
-            elif self.left is None:
-                return self.right
-            elif self.right is None:
-                return self.right
-
-            max_val = self.left.find_max()
-            self.data = max_val
-            self.left = self.left.delete(max_val)
-
-        return self
-
-    def find_max(self):
-        if self.right is None:
-            return self.data
-        return self.right.find_max()
-
-    def find_min(self):
-        if self.left is None:
-            return self.data
-        return self.left.find_min()
-
-
-def build_tree(elements):
-    print("Building tree with these elements:",elements)
-    root = BinarySearchTreeNode(elements[0])
-
-    for i in range(1,len(elements)):
-        root.add_child(elements[i])
-
-    return root
-
-if __name__ == '__main__':
-    numbers_tree = build_tree([17, 4, 1, 20, 9, 23, 18, 34])
-    numbers_tree.delete(20)
-    print("After deleting 20 ",numbers_tree.in_order_traversal()) # this should print [1, 4, 9, 17, 18, 23, 34]
-
-    numbers_tree = build_tree([17, 4, 1, 20, 9, 23, 18, 34])
-    numbers_tree.delete(9)
-    print("After deleting 9 ",numbers_tree.in_order_traversal())  # this should print [1, 4, 17, 18, 20, 23, 34]
-
-    numbers_tree = build_tree([17, 4, 1, 20, 9, 23, 18, 34])
-    numbers_tree.delete(17)
-    print("After deleting 17 ",numbers_tree.in_order_traversal())  # this should print [1, 4, 9, 18, 20, 23, 34]
-
-
-#################### LESSON 12 ####################
-
-class Graph:
-    def __init__(self, edges):
-        self.edges = edges
-        self.graph_dict = {}
-        for start, end in edges:
-            if start in self.graph_dict:
-                self.graph_dict[start].append(end)
-            else:
-                self.graph_dict[start] = [end]
-        print("Graph Dict:", self.graph_dict)
-
-    def get_paths(self, start, end, path=[]):
-        path = path + [start]
-
-        if start == end:
-            return [path]
-
-        if start not in self.graph_dict:
-            return []
-
-        paths = []
-        for node in self.graph_dict[start]:
-            if node not in path:
-                new_paths = self.get_paths(node, end, path)
-                for p in new_paths:
-                    paths.append(p)
-
-        return paths
-
-    def get_shortest_path(self, start, end, path=[]):
-        path = path + [start]
-
-        if start == end:
-            return path
-
-        if start not in self.graph_dict:
-            return None
-
-        shortest_path = None
-        for node in self.graph_dict[start]:
-            if node not in path:
-                sp = self.get_shortest_path(node, end, path)
-                if sp:
-                    if shortest_path is None or len(sp) < len(shortest_path):
-                        shortest_path = sp
-
-        return shortest_path
-
-if __name__ == '__main__':
-
-    routes = [
-        ("Mumbai","Pune"),
-        ("Mumbai", "Surat"),
-        ("Surat", "Bangaluru"),
-        ("Pune","Hyderabad"),
-        ("Pune","Mysuru"),
-        ("Hyderabad","Bangaluru"),
-        ("Hyderabad", "Chennai"),
-        ("Mysuru", "Bangaluru"),
-        ("Chennai", "Bangaluru")
-    ]
-
-    routes = [
-        ("Mumbai", "Paris"),
-        ("Mumbai", "Dubai"),
-        ("Paris", "Dubai"),
-        ("Paris", "New York"),
-        ("Dubai", "New York"),
-        ("New York", "Toronto"),
-    ]
-
-    route_graph = Graph(routes)
-
-    start = "Mumbai"
-    end = "New York"
-
-    print(f"All paths between: {start} and {end}: ",route_graph.get_paths(start,end))
-    print(f"Shortest path between {start} and {end}: ", route_graph.get_shortest_path(start,end))
-
-    start = "Dubai"
-    end = "New York"
-
-    print(f"All paths between: {start} and {end}: ",route_graph.get_paths(start,end))
-    print(f"Shortest path between {start} and {end}: ", route_graph.get_shortest_path(start,end))
-
-
-#################### LESSON 13 ####################
-
-from util import time_it
-
-@time_it
-def linear_search(numbers_list, number_to_find):
-    for index, element in enumerate(numbers_list):
-        if element == number_to_find:
-            return index
-    return -1
-
-@time_it
-def binary_search(numbers_list, number_to_find):
-    left_index = 0
-    right_index = len(numbers_list) - 1
-    mid_index = 0
-
-    while left_index <= right_index:
-        mid_index = (left_index + right_index) // 2
-        mid_number = numbers_list[mid_index]
-
-        if mid_number == number_to_find:
-            return mid_index
-
-        if mid_number < number_to_find:
-            left_index = mid_index + 1
-        else:
-            right_index = mid_index - 1
-
-    return -1
-
-def binary_search_recursive(numbers_list, number_to_find, left_index, right_index):
-    if right_index < left_index:
-        return -1
-
-    mid_index = (left_index + right_index) // 2
-    if mid_index >= len(numbers_list) or mid_index < 0:
-        return -1
-
-    mid_number = numbers_list[mid_index]
-
-    if mid_number == number_to_find:
-        return mid_index
-
-    if mid_number < number_to_find:
-        left_index = mid_index + 1
-    else:
-        right_index = mid_index - 1
-
-    return binary_search_recursive(numbers_list, number_to_find, left_index, right_index)
-
-if __name__ == '__main__':
-    numbers_list = [12, 15, 17, 19, 21, 24, 45, 67]
-    number_to_find = 21
-
-    index = binary_search_recursive(numbers_list, number_to_find, 0, len(numbers_list))
-    print(f"Number found at index {index} using binary search")
-
-
-
-
-
-
-
-def binary_search(numbers_list, number_to_find):
-    left_index = 0
-    right_index = len(numbers_list) - 1
-    mid_index = 0
-
-    while left_index <= right_index:
-        mid_index = (left_index + right_index) // 2
-        mid_number = numbers_list[mid_index]
-
-        if mid_number == number_to_find:
-            return mid_index
-
-        if mid_number < number_to_find: # this means number is in right hand side of the list
-            left_index = mid_index + 1
-        else: # number to find is on left hand side of the list
-            right_index = mid_index - 1
-
-    return -1
-
-def find_all_occurances(numbers, number_to_find):
-    index = binary_search(numbers, number_to_find)
-    indices = [index]
-    # find indices on left hand side
-    i = index-1
-    while i >=0:
-        if numbers[i] == number_to_find:
-            indices.append(i)
-        else:
-            break
-        i = i - 1
-
-    # find indices on right hand side
-    i = index + 1
-    while i<len(numbers):
-        if numbers[i] == number_to_find:
-            indices.append(i)
-        else:
-            break
-        i = i + 1
-
-    return sorted(indices)
+def insertion_sort(elements):
+    for i in range(1, len(elements)):
+        anchor = elements[i]
+        j = i - 1
+        while j>=0 and anchor < elements[j]:
+            elements[j+1] = elements[j]
+            j = j-1
+        elements[j+1] = anchor
+                   
 
 
 if __name__ == '__main__':
-    numbers = [1,4,6,9,11,15,15,15,17,21,34,34,56]
-    number_to_find = 15
-    indices = find_all_occurances(numbers, number_to_find)
-    print(f"Indices of occurances of {number_to_find} are {indices}")
-
-#################### LESSON 14 ####################
-
-def bubble_sort(elements):
-    size = len(elements)
-    for i in range(size-1):
-        swapped = False
-        for j in range(size-1-i):
-
-            if elements[j] > elements[j+1]:
-                tmp = elements[j]
-                elements[j] = elements[j+1]
-                elements[j+1] = tmp
-                swapped = True
-        if not swapped:
-            break
-
-if __name__ == '__main__':
-    #elements = [5,9,2,1,67,34,88,34]
-    elements = [1,2,4]
-
-    bubble_sort(elements)
+    elements = [11,9,29,7,2,15,28]
+    insertion_sort(elements)
     print(elements)
+
+
+
+
+
+
+def place_to_insert(array, key):
+    index = 0
+    for i in array:
+        if i > key:
+            break
+        else:
+            index += 1
+    return index
+
+
+def insert_to_sorted(array, key):
+    index = place_to_insert(array, key)
+    return array[0:index]+[key]+array[index:]
+
+
+if __name__ == "__main__":
+    array = [2, 1, 5, 7, 2, 0, 5]
+
+    stream = []
+
+    count = 0
+    while(True):
+        i = int(input())
+        count += 1
+        stream = insert_to_sorted(stream, i)
+        if count % 2 == 1:
+            print(f"Median of {stream} : {stream[(count)//2]}")
+        else:
+            i1 = count//2
+            i2 = (count//2) - 1
+            print(f"Median of {stream} : {(stream[i1] + stream[i2])/2}")
