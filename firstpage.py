a9df51eedf7dc79f4640a3d70ab55b119be7067c
Ibrahim <ibrahimzaky2000@gmail.com>
2025-05-12 19:08:57 +0200

    firstpage.py

--- firstpage.py
+++ firstpage.py
@@ -1,4 +1,35 @@
 ######################## LESSON 15 ########################
+def swap(a, b, arr): 
+    if a != b:
+        tmp = arr[a]
+        arr[a] = arr[b]
+        arr[b] = tmp
+
+def partition(elements, start, end):
+    pivot_index = start
+    pivot = elements[pivot_index]
+
+    while start < end:
+        while start < len(elements) and elements[start] <= pivot:
+            start += 1
+        while elements[end] > pivot:
+            end -= 1
+        if start < end:
+            swap(start, end, elements)
+
+    swap(pivot_index, end, elements)
+    return end
+
+def quick_sort(elements, start, end):
+    if start < end:
+        pi = partition(elements, start, end)
+        quick_sort(elements, start, pi - 1)
+        quick_sort(elements, pi + 1, end)
+
+if __name__ == '__main__':
+    elements = [11, 9, 29, 7, 2, 15, 28]
+    quick_sort(elements, 0, len(elements) - 1)
+    print(elements)
 
 
 
@@ -57,3 +88,6 @@ if __name__ == "__main__":
             i1 = count//2
             i2 = (count//2) - 1
             print(f"Median of {stream} : {(stream[i1] + stream[i2])/2}")
+
+
+
