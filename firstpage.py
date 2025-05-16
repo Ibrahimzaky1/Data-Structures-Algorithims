8dd14afa3324af1e50c5da26a0bcdc16ba0c54b4
Ibrahim <ibrahimzaky2000@gmail.com>
2025-05-16 18:44:50 +0200

    Updated firstpage.py

--- firstpage.py
+++ firstpage.py
@@ -1,45 +1,97 @@
+##################################### LESSON 18 #####################################
+def shell_sort(arr):
+    size = len(arr)
+    gap = size // 2
+    while gap > 0:
+        for i in range(gap,size):
+            anchor = arr[i]
+            j = i
+            while j>=gap and arr[j-gap] > anchor:
+                arr[j] = arr[j-gap]
+                j -= gap
 
-########################### LESSON 17 ###########################
-def merge_sort(arr):
-    if len(arr) <= 1:
-        return arr
+            arr[j] = anchor
+        gap = gap // 2
 
-    mid = len(arr) // 2  # Calculate midpoint first
-    left = arr[:mid]     # Split into left and right
-    right = arr[mid:]
 
-    left = merge_sort(left)   # Recursively sort left
-    right = merge_sort(right) # Recursively sort right
-
-    return merge_two_sorted_lists(left, right)  # Merge the sorted halves
-
-def merge_two_sorted_lists(a, b):
-    sorted_list = []
-    len_a = len(a)
-    len_b = len(b)
-    i = j = 0
-
-    while i < len_a and j < len_b:
-        if a[i] <= b[j]:
-            sorted_list.append(a[i])
-            i += 1
-        else:
-            sorted_list.append(b[j])
-            j += 1
-
-    while i < len_a:
-        sorted_list.append(a[i])
-        i += 1
-
-    while j < len_b:  # Fixed: use `j`, not `i`
-        sorted_list.append(b[j])
-        j += 1
-
-    return sorted_list
 
 if __name__ == '__main__':
-    arr = [10, 3, 15, 7, 8, 23, 98, 29]
-    print(merge_sort(arr))
+    elements = [21,36,29,17,4,25,11,32,9]
+    shell_sort(elements)
+    print(elements)
+
+
+
+
+
+
+def shell_sort(arr):
+    n = len(arr)
+    div = 2
+    gap = n//div
+    while gap > 0:
+        index_to_delete = []
+        for i in range(gap, n):
+            temp = arr[i]
+            j = i
+            while j >= gap and arr[j-gap] >= temp:
+                if arr[j-gap] == temp:
+                    index_to_delete.append(j)
+                arr[j] = arr[j-gap]
+                j -= gap
+            arr[j] = temp
+        index_to_delete=list(set(index_to_delete))
+        index_to_delete.sort()
+        if index_to_delete:
+            for i in index_to_delete[-1::-1]:
+                del arr[i]
+        div *= 2
+        n = len(arr)
+        gap = n//div
+
+
+if __name__ == '__main__':
+    elements = [2, 1, 5, 7, 2, 0, 5, 1, 2, 9,  5, 8, 3]
+
+    print(f'Given unsorted list: {elements}')
+    shell_sort(elements)
+    print(f'List after Sorting : {elements}')
+
+
+
+##################################### LESSON 19 ##################################### 
+
+def find_min_element(arr):
+    min = 100000000
+    for i in range(len(arr)):
+        if arr[i] < min:
+            min = arr[i]
+        return min
+
+
+if __name__ == '__main__':
+    elements = [78,12,15,8,61,53,23,27]
+    print(find_min_element(elements))
+
+
+
+def selection_sort(arr):
+    size = len(arr)
+    for i in range(size-1):
+        min_index = i
+        for j in range(min_index+1, size):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        if i != min_index:
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+
+
+
+
+if __name__ == '__main__':
+    elements = [78,12,15,8,61,53,23,27]
+    selection_sort(elements)
+    print(elements)
 
 
 
@@ -47,48 +99,36 @@ if __name__ == '__main__':
 
 
 
+def multilevel_selection_sort(elements, sort_by_list):
+    for sort_by in sort_by_list[-1::-1]:
+        for x in range(len(elements)):
+            min_index = x
+            for y in range(x, len(elements)):
+                if elements[y][sort_by] < elements[min_index][sort_by]:
+                    min_index = y
+            if x != min_index:
+                elements[x], elements[min_index] = elements[min_index], elements[x]
 
-
-def merge_sort(elements, key, descending=False):
-    size = len(elements)
-
-    if size == 1:
-        return elements
-
-    left_list = merge_sort(elements[0:size//2], key, descending)
-    right_list = merge_sort(elements[size//2:], key, descending)
-    sorted_list = merge(left_list, right_list, key, descending)
-
-    return sorted_list
-
-    
-def merge(left_list, right_list, key, descending=False):
-    merged = []
-    if descending:
-        while len(left_list) > 0 and len(right_list) > 0:
-            if left_list[0][key] >= right_list[0][key]:
-                merged.append(left_list.pop(0))
-            else:
-                merged.append(right_list.pop(0))
-
-    else:
-        while len(left_list) > 0 and len(right_list) > 0:
-            if left_list[0][key] <= right_list[0][key]:
-                merged.append(left_list.pop(0))
-            else:
-                merged.append(right_list.pop(0))
-
-    merged.extend(left_list)
-    merged.extend(right_list)
-    return merged
 
 if __name__ == '__main__':
     elements = [
-        { 'name': 'vedanth',   'age': 17, 'time_hours': 1},
-        { 'name': 'rajab', 'age': 12,  'time_hours': 3},
-        { 'name': 'vignesh',  'age': 21,  'time_hours': 2.5},
-        { 'name': 'chinmay',  'age': 24,  'time_hours': 1.5},
+        {'First Name': 'Raj', 'Last Name': 'Nayyar'},
+        {'First Name': 'Suraj', 'Last Name': 'Sharma'},
+        {'First Name': 'Karan', 'Last Name': 'Kumar'},
+        {'First Name': 'Jade', 'Last Name': 'Canary'},
+        {'First Name': 'Raj', 'Last Name': 'Thakur'},
+        {'First Name': 'Raj', 'Last Name': 'Sharma'},
+        {'First Name': 'Kiran', 'Last Name': 'Kamla'},
+        {'First Name': 'Armaan', 'Last Name': 'Kumar'},
+        {'First Name': 'Jaya', 'Last Name': 'Sharma'},
+        {'First Name': 'Ingrid', 'Last Name': 'Galore'},
+        {'First Name': 'Jaya', 'Last Name': 'Seth'},
+        {'First Name': 'Armaan', 'Last Name': 'Dadra'},
+        {'First Name': 'Ingrid', 'Last Name': 'Maverick'},
+        {'First Name': 'Aahana', 'Last Name': 'Arora'}
     ]
 
-    sorted_list = merge_sort(elements, key='time_hours', descending=True)
-    print(sorted_list)
+    print(f'Given unsorted array:', *elements, sep='\n')
+    multilevel_selection_sort(
+        elements, ['First Name', 'Last Name'])
+    print(f'Array after Multi-Level Sorting:', *elements, sep='\n')
