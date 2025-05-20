2b9f7df6568b4038d5f2f7d08a29df56419f9f3a
Ibrahim <ibrahimzaky2000@gmail.com>
2025-05-20 18:23:01 +0200

    Updated firstpage.py

--- firstpage.py
+++ firstpage.py
@@ -1,149 +1,94 @@
-##################################### LESSON 20 ##################################### 
-def fib(n):
-    if n==0 or n==1:
-        return n
-    return fib(n-1) + fib(n-2)
+class singlyNode:  # ❌ You wrote ".class", which is invalid
+    def __init__(self, val, next=None):
+        self.val = val  # ❌ You forgot to assign self.val
+        self.next = next
+
+    def __str__(self):
+        return str(self.val)
+
+# Creating nodes
+Head = singlyNode(1)
+A = singlyNode(3)
+B = singlyNode(4)
+C = singlyNode(7)
+
+# Linking nodes
+Head.next = A
+A.next = B
+B.next = C
+
+# Print head value
+print(Head)
+
+# Traverse and print each node
+curr = Head
+while curr:
+    print(curr)
+    curr = curr.next
+
+# Display function
+def display(Head):
+    curr = Head
+    elements = []
+    while curr:
+        elements.append(str(curr.val))
+        curr = curr.next
+    print(' -> '.join(elements))
+
+display(Head)
+
+# ❌ You forgot the colon (:) in the function definition
+def search(Head, val):
+    curr = Head
+    while curr:
+        if val == curr.val:
+            return True
+        curr = curr.next
+    return False
+
+# Call and print the result of search
+print(search(Head, 2))  # This will print False
 
 
 
-if __name__=='__main__':
-    print(fib(4))
+class DoublyNodes:
+    def __init__(self, val, next=None, prev=None):
+        self.val = val
+        self.next = next
+        self.prev = prev
 
+    def __str__(self):
+        return str(self.val)
+    
 
-##################################### EXCERCISE 1 #####################################
+head = tail = DoublyNodes(1)
+print(tail)
 
+def display(head):
+    curr = head
+    elements = []
+    while curr:
+        elements.append(str(curr.val))
+        curr = curr.next
+    print(' <-> '.join(elements))
 
-def list_sum(num_List):
-    if len(num_List) == 1:
-        return num_List[0]
-    else:
-        return num_List[0] + list_sum(num_List[1:])
+display(head)
 
-print(list_sum([2, 4, 5, 6, 7]))
+def insert_at_beginnig(head, tail, val):
+    new_node = DoublyNodes(val, next=head)
+    head.prev = new_node
+    return new_node, tail
 
+head, tail = insert_at_beginnig(head, tail, 3)
+display(head)
 
-##################################### EXCERCISE 2 #####################################
+def insert_at_end(head, tail, val):
+    new_node = DoublyNodes(val, prev=tail)
+    tail.next = new_node
+    return head, new_node
 
-def to_string(n, base):
-    conver_tString = "0123456789ABCDEF"
-    if n < base:
-        return conver_tString[n]
-    else:
-        return to_string(n // base, base) + conver_tString[n % base]
+head, tail = insert_at_end(head, tail, 7)
+display(head)
 
-print(to_string(2835, 16))
+####################### Lesson 4 #######################
 
-
-##################################### EXCERCISE 3 #####################################
-
-
-def recursive_list_sum(data_list):
-    total = 0
-    for element in data_list:
-        if type(element) == type([]):
-            total = total + recursive_list_sum(element)
-        else:
-            total = total + element
-
-    return total
-
-print(recursive_list_sum([1, 2, [3, 4], [5, 6]]))
-
-##################################### EXCERCISE 4 #####################################
-
-
-def factorial(n):
-    if n <= 1:
-        return 1
-    else:
-        return n * factorial(n - 1)
-print(factorial(5))
-
-##################################### EXCERCISE 5 #####################################
-
-def fibonacci(n):
-    if n == 1 or n == 2:
-        return 1
-    else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
-print(fibonacci(7))
-
-
-##################################### EXCERCISE 6 #####################################
-
-
-def sumDigits(n):
-    if n == 0:
-        return 0
-    else:
-        return n % 10 + sumDigits(int(n / 10))
-print(sumDigits(345))
-print(sumDigits(45))
-
-
-
-##################################### EXCERCISE 7 #####################################
-
-
-def sum_series(n):
-    if n < 1:
-        return 0
-    else:
-        return n + sum_series(n - 2)
-print(sum_series(6))
-print(sum_series(10))
-
-
-##################################### EXCERCISE 8 #####################################
-
-
-
-def harmonic_sum(n):
-    if n < 2:
-        return 1
-    else:
-        return 1 / n + harmonic_sum(n - 1)
-print(harmonic_sum(7))
-print(harmonic_sum(4))
-
-
-##################################### EXCERCISE 9 #####################################
-
-
-def geometric_sum(n):
-    if n == 0:
-        return 1
-    else:
-        return 1 / (pow(2, n)) + geometric_sum(n - 1)
-print(geometric_sum(7))
-print(geometric_sum(4))
-
-
-##################################### EXCERCISE 10 #####################################
-
-
-def power(a, b):
-    if b == 0:
-        return 1
-    elif a == 0:
-        return 0
-    elif b == 1:
-        return a
-    else:
-        return a * power(a, b - 1)
-print(power(3, 4))
-
-
-
-##################################### EXCERCISE 11 #####################################
-
-def Recurgcd(a, b):
-    low = min(a, b)
-    high = max(a, b)
-    if low == 0:
-        return high
-    elif low == 1:
-        return 1
-    else:
-        return Recurgcd(low, high % low)
-print(Recurgcd(12, 14))
