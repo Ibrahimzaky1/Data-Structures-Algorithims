52ab3df66df74118c75193914e82fee44fb64a50
Ibrahim <ibrahimzaky2000@gmail.com>
2025-06-07 18:39:29 +0200

    Updated firstpage.py

--- firstpage.py
+++ firstpage.py
@@ -1,93 +1,3 @@
-
-from typing import List
-
-class Solution:
-    def sortedSquares(self, nums: List[int]) -> List[int]:
-        left = 0
-        right = len(nums) - 1
-        result = []
-
-        while left <= right:
-            if abs(nums[left]) > abs(nums[right]):
-                result.append(nums[left] ** 2)
-                left += 1
-            else:
-                result.append(nums[right] ** 2)
-                right -= 1
-
-        result.reverse()
-        return result
-    
-
-# Example usage
-nums = [-7, -3, 2, 3, 11]
-solution = Solution()
-print(solution.sortedSquares(nums))  # Output: [4,]()
-
-                              
-
-
-from typing import List
-
-class Solution:
-    def summaryRanges(self, nums: List[int]) -> List[str]:
-        ans = []
-        i = 0
-
-        while i < len(nums):
-            start = nums[i]  # fix 1: get the starting number
-            while i < len(nums) - 1 and nums[i] + 1 == nums[i + 1]:
-                i += 1
-
-            if start != nums[i]:
-                ans.append(f"{start}->{nums[i]}")
-            else:
-                ans.append(str(nums[i]))
-
-            i += 1  # fix 2: move to the next number
-
-        return ans
-
-# Example usage
-solution = Solution()
-print(solution.summaryRanges([0, 1, 2, 4, 5, 7]))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-from typing import List
-
-class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        l_mult = 1
-        r_mult = 1
-        n = len(nums)
-        l_arr = [0] * n
-        r_arr = [0] * n
-
-        for i in range(n):
-            j = -i - 1
-            l_arr[i] = l_mult
-            r_arr[j] = r_mult
-            l_mult *= nums[i]
-            r_mult *= nums[j]
-
-        return [l * r for l, r in zip(l_arr, r_arr)]
-
-# Example usage:
-sol = Solution()
-print(sol.productExceptSelf([1, 2, 3, 4]))  # Output: [24, 12, 8, 6]
-
 from typing import List
 
 class Solution:
@@ -115,4 +25,56 @@ print(result)  # Output: [[1, 6], [8, 10], [15, 18]]
 
         
          
-        
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        m, n = len(matrix), len(matrix[0])
+        ans = []
+        i, j = 0, 0
+        UP, RIGHT, DOWN, LEFT, = 0, 1, 2, 3
+        direction = RIGHT
+
+        UP_WALL = 0
+        RIGHT_WALL = n
+        DOWN_WALL = m
+        LEFT_WALL = -1
+
+        while len(ans) != m*n:
+            if direction == RIGHT :
+                while j < RIGHT_WALL:
+                    ans.append(matrix[i][j])
+                    j += 1
+                i, j = i+1, j-1
+                RIGHT_WALL -= 1
+                direction = DOWN
+
+            elif direction == DOWN:
+                while i < DOWN_WALL:
+                    ans.append(matrix[i][j])
+                    i += 1
+                i, j = i-1, j-1
+                DOWN_WALL -= 1
+                direction = LEFT
+            elif direction == LEFT:
+                while j > LEFT_WALL:
+                    ans.append(matrix[i][j])
+                    j -= 1
+                i, j = i-1, j+1
+                LEFT_WALL += 1
+                direction = UP
+            else:
+                while i > UP_WALL:
+                    ans.append(matrix[i][j])
+                    i -= 1
+                i, j = i+1, j+1
+                UP_WALL += 1
+                direction = RIGHT
+
+        return ans
+
+sol = Solution()
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print(sol.spiralOrder(matrix))                 
