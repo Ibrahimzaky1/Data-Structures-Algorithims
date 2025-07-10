07af2ff0288c6e5018c15e757b0fdd8dc2fc631f
Ibrahim <ibrahimzaky2000@gmail.com>
2025-07-10 17:12:36 +0200

    Updated firstpage.py

--- firstpage.py
+++ firstpage.py
@@ -1,36 +1,106 @@
 from typing import List
-from collections import defaultdict
-import heapq
 
 class Solution:
-    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        graph = defaultdict(list)
-        for u, v, time in times:
-            graph[u].append((v, time))
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        n = len(nums)
+        closest_sum = nums[0] + nums[1] + nums[2]  
 
-        min_times = {}
-        min_heap = [(0, k)]
+        for i in range(n - 2):  
+            lo, hi = i + 1, n - 1
+            while lo < hi:
+                cur_sum = nums[i] + nums[lo] + nums[hi]
 
-        while min_heap:
-            time_k_to_i, i = heapq.heappop(min_heap)
-            if i in min_times:
-                continue
+                if abs(cur_sum - target) < abs(closest_sum - target):
+                    closest_sum = cur_sum
 
-            min_times[i] = time_k_to_i
-            for nei, nei_time in graph[i]:
-                if nei not in min_times:
-                    heapq.heappush(min_heap, (time_k_to_i + nei_time, nei))
+                if cur_sum == target:
+                    return cur_sum
+                elif cur_sum < target:
+                    lo += 1
+                else:
+                    hi -= 1
 
-        if len(min_times) == n:
-            return max(min_times.values())  
-        else:
-            return -1
+        return closest_sum
 
 
 sol = Solution()
-print(sol.networkDelayTime([
-    [2, 1, 1],
-    [2, 3, 1],
-    [3, 4, 1]
-], 4, 2))  
+print(sol.threeSumClosest([-1, 2, 1, -4], 1))  
+
+
+
+
+class Solution:
+    def fib(self, n: int) -> int:
+        memo = {0: 0, 1: 1}
+
+        def f(x):
+            if x in memo:
+                return memo[x]
+            memo[x] = f(x - 1) + f(x - 2)
+            return memo[x]
+
+        return f(n)
+
+
+sol = Solution()
+print(sol.fib(10))  
+
+
+
+
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+
+        prev, cur = 0, 1
+        for _ in range(2, n + 1):
+            prev, cur = cur, prev + cur
+
+        return cur
+sol = Solution()
+print(sol.fib(10))  
+
+    
+
+
+class Solution:
+    def climStairs(self, n: int) -> int:
+        
+        memo = {1:1, 2:2}
+        def f(n):   
+            if n in memo:
+                return memo[n]
+            else:
+                memo[n] = f(n-2) + f(n-1)
+                return memo[n]
+            
+        return f(n)
+    
+
+
+
+class Solution:
+    def climStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        
+        dp = [0] * n
+        dp[0] = 1
+        dp[1] = 2
+
+        for i in range(2, n):
+            dp[i] = dp[i-2] + dp[i-1]
+
+        return dp[n-1]
+
+
+sol = Solution()
+print(sol.climStairs(5))  
+
 
