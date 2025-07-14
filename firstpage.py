fd7867a5bc309ebb887e538fc5328bc1241275cb
Ibrahim <ibrahimzaky2000@gmail.com>
2025-07-14 18:16:36 +0200

    Updated firstpage.py

--- firstpage.py
+++ firstpage.py
@@ -1,72 +1,33 @@
 from typing import List
 
 class Solution:
-    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        n = len(nums)
-        answer = []
-        nums.sort()
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        n = len(heights)
+        stk = []
+        max_area = 0
 
-        for i in range(n):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
+        for i, height in enumerate(heights):
+            start = i
+            while stk and height < stk[-1][0]:
+                h, j = stk.pop()
+                w = i - j
+                a = h * w
+                max_area = max(max_area, a)
+                start = j
 
-            for j in range(i + 1, n):
-                if j > i + 1 and nums[j] == nums[j - 1]:
-                    continue
+            stk.append((height, start))
 
-                lo, hi = j + 1, n - 1
+        while stk:
+            h, j = stk.pop()
+            w = n - j
+            max_area = max(max_area, h * w)
 
-                while lo < hi:
-                    summ = nums[i] + nums[j] + nums[lo] + nums[hi]
-
-                    if summ == target:
-                        answer.append([nums[i], nums[j], nums[lo], nums[hi]])
-                        lo += 1
-                        hi -= 1
-
-                        while lo < hi and nums[lo] == nums[lo - 1]:
-                            lo += 1
-                        while lo < hi and nums[hi] == nums[hi + 1]:
-                            hi -= 1
-
-                    elif summ < target:
-                        lo += 1
-                    else:
-                        hi -= 1
-
-        return answer
+        return max_area
 
 
 sol = Solution()
-print(sol.fourSum([1, 0, -1, 0, -2, 2], 0))
+print(sol.largestRectangleArea([2, 1, 5, 6, 2, 3]))  
 
-
-
-
-class Solution:
-    def threeSumClosest(self, nums: List[int], target: int) -> int:
-        nums.sort()
-        n = len(nums)
-        closest_sum = float('inf')
-
-        for i in range(n):
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-            lo, hi = i+1, n-1
-            while lo < hi:
-                cur_sum = nums[i] + nums[0] + nums[hi]
-                if abs(cur_sum - target) < abs(closest_sum - target):
-                    closest_sum = cur_sum
-
-                if cur_sum == target:
-                    return cur_sum
-                elif cur_sum < target:
-                    lo += 1
-                else:
-                    hi -= 1
-
-        return closest_sum
-    
 
 
 
@@ -74,21 +35,158 @@ class Solution:
 from typing import List
 
 class Solution:
-    def sortColors(self, nums: List[int]) -> None:
-        counts = [0, 0, 0]
-        for color in nums:
-            counts[color] += 1
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        x, y = m - 1, n - 1
 
-        R, W, B = counts
-        nums[:R] = [0] * R
-        nums[R:R + W] = [1] * W
-        nums[R + W:] = [2] * B
+        for z in range(m + n - 1, -1, -1):
+            if x < 0:
+                nums1[z] = nums2[y]
+                y -= 1
+            elif y < 0:
+                break
+            elif nums1[x] > nums2[y]:
+                nums1[z] = nums1[x]
+                x -= 1
+            else:
+                nums1[z] = nums2[y]
+                y -= 1
 
+
+nums1 = [1, 2, 3, 0, 0, 0]
+m = 3
+nums2 = [2, 5, 6]
+n = 3
+
+sol = Solution()
+sol.merge(nums1, m, nums2, n)
+print(nums1)  
+
+
+
+
+
+
+
+
+from typing import List
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        n = len(nums)
+
+        while i < n:
+            if nums[i] == val:
+                nums[i] = nums[n - 1]
+                n -= 1
+            else:
+                i += 1
+
+        return n
+
+nums = [3, 2, 2, 3]
+val = 3
+
+sol = Solution()
+k = sol.removeElement(nums, val)
+print("New length:", k)
+print("Modified list:", nums[:k])  
+
+
+
+
+
+
+
+from typing import List
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        n = len(nums)
+        j = 1
+
+        for i in range(1, n):
+            if nums[i] != nums[i-1]:
+                nums[j] = nums[i]
+                j += 1
+
+        return j
 
 
 sol = Solution()
-nums = [2, 0, 2, 1, 1, 0]
-sol.sortColors(nums)
-print(nums)  
+nums = [0,0,1,1,1,2,2,3,3,4]
+length = sol.removeDuplicates(nums)
+print("Length after removing duplicates:", length)
+print("Modified array:", nums[:length])
 
 
+
+
+
+
+
+from typing import List
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        j = 1
+        count = 1
+        n = len(nums)
+
+        for i in range(1, n):
+            if nums[i] == nums[i - 1]:
+                count += 1
+            else:
+                count = 1
+
+            if count <= 2:
+                nums[j] = nums[i]
+                j += 1
+
+        return j
+
+
+sol = Solution()
+nums = [0,0,1,1,1,2,2,3,3,4]
+length = sol.removeDuplicates(nums)
+print("Length after removing duplicates:", length)
+print("Modified array:", nums[:length])
+
+    
+
+
+
+
+
+
+
+
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        i = 0
+        profit = 0
+        n = len(prices)
+
+        while i < n - 1:
+            while i < n - 1 and prices[i] >= prices[i + 1]:
+                i += 1
+            if i == n - 1:
+                break
+            lo = prices[i]
+            i += 1
+
+            
+            while i < n and prices[i] >= prices[i - 1]:
+                i += 1
+            hi = prices[i - 1]
+
+            profit += hi - lo
+
+        return profit
+
+
+sol = Solution()
+prices = [7,1,5,3,6,4]
+print("Max Profit:", sol.maxProfit(prices))
